88afab0c45755331f2e9c1a275a91ebc685ed031
Lucas van Dijk <info@return1.net>
2014-01-25 19:11:55 +0100

    Fixed static files for development

--- jd_projects_site/settings.py
+++ jd_projects_site/settings.py
@@ -83,8 +83,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "jd_projects_site", "static")
+#STATIC_ROOT = os.path.join(BASE_DIR, "jd_projects_site", "static")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "jd_projects_site", "static"),
+)
 
 # Extra template directory
 TEMPLATE_DIRS = (
--- jd_projects_site/urls.py
+++ jd_projects_site/urls.py
@@ -1,10 +1,15 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url, static
 from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = []
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += patterns('',
     # Examples:
     # url(r'^$', 'jd_projects.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -14,5 +19,3 @@ urlpatterns = patterns('',
     url(r'^projects/', include('jd_projects.urls'))
 )
 
-if settings.DEBUG:
-   urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
