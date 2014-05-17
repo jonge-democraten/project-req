7beeb8a960a2a9693b540bd00321e6319c32b0dc
Lucas van Dijk <info@return1.net>
2014-05-17 12:48:06 +0200

    Proper form labels for the models

--- jd_projects/models.py
+++ jd_projects/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext_lazy as _
 
 class Project(models.Model):
-    project_title = models.CharField(max_length=200)
+    project_title = models.CharField(_("project titel"), max_length=200)
     project_slug = models.SlugField(max_length=255)
-    project_org = models.CharField(max_length=200)
-    project_location = models.CharField(max_length=255)
-    project_date = models.DateTimeField()
-    project_desc = models.TextField(null=True)
-    project_goal = models.TextField(null=True, blank=True)
+    project_org = models.CharField(_("organisatie"), max_length=200)
+    project_location = models.CharField(_("locatie"), max_length=255)
+    project_date = models.DateTimeField(_("datum (yyyy-mm-dd)"))
+    project_desc = models.TextField(_("Beschrijving"), null=True)
+    project_goal = models.TextField(_("Doel"), null=True, blank=True)
 
-    requester_org = models.CharField(max_length=200)
-    requester_name = models.CharField(max_length=200)
-    requester_title = models.CharField(max_length=200)
-    requester_address = models.CharField(max_length=200)
-    requester_postcode = models.CharField(max_length=6)
-    requester_city = models.CharField(max_length=50)
-    requester_email = models.EmailField()
-    requester_phone = models.CharField(max_length=15)
+    requester_org = models.CharField(_("organisatie"), max_length=200)
+    requester_name = models.CharField(_("naam"), max_length=200)
+    requester_title = models.CharField(_("titel"), max_length=200)
+    requester_address = models.CharField(_("adres"), max_length=200)
+    requester_postcode = models.CharField(_("postcode"), max_length=6)
+    requester_city = models.CharField(_("stad"), max_length=50)
+    requester_email = models.EmailField(_("email"))
+    requester_phone = models.CharField(_("telefoon"), max_length=15)
 
     def save(self, *args, **kwargs):
         self.project_slug = slugify("{}-{}".format(self.project_org, self.project_title))
@@ -28,8 +29,8 @@ class Project(models.Model):
 
 class ProjectIncomeExpenses(models.Model):
     project = models.ForeignKey('Project')
-    description = models.CharField(max_length=255)
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.CharField(_("beschrijving"), max_length=255)
+    amount = models.DecimalField(_("bedrag"), max_digits=6, decimal_places=2)
 
     def __unicode__(self):
         return "{} - {:.2f}".format(self.description, self.amount)
